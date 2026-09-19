2b8a0c506d20155d82fdcc05733382b7fbb0215e
bru-reddy <maligireddysaibrundha@gmail.com>
2026-09-19 18:21:22 +0530

    Fix shared authentication and cart state

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,1 +1,1 @@
-import React from 'react';import{createRoot}from'react-dom/client';import{BrowserRouter}from'react-router-dom';import App from './App.jsx';createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>);
+import React from 'react';import{createRoot}from'react-dom/client';import{BrowserRouter}from'react-router-dom';import App,{Providers}from'./App.jsx';createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><Providers><App/></Providers></BrowserRouter></React.StrictMode>);
